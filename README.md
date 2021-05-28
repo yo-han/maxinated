f8256808ff74177d236dc8a1415b43b42313b460
maartendek <dekker.maarten@gmail.com>
2021-05-28 15:22:56 +0200

    vue cli init

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 Ben jij al #maxinated?
 
 <img width="572" alt="Schermafbeelding 2021-05-28 om 12 18 10" src="https://user-images.githubusercontent.com/197600/119969399-cda09500-bfae-11eb-8334-7e702401530d.png">
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>De festivalzomer lacht je toe!</h1>
+    <h3>Kijk ook hier:</h3>
+    <ul>
+      <li><a href="https://coronatest.nl/" target="_blank" rel="noopener">Coronatest.nl</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
